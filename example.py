04e2c311e81d3c495cc393ed799c8a48e4f4da29
A3R0 <50676042+hostinfodev@users.noreply.github.com>
2021-12-26 23:16:35 -0800

    Update example.py

--- example.py
+++ example.py
@@ -12,10 +12,14 @@ def onReady(tools):
 def messageHandler_all(ws, message):
     print("\n\nGOT MESSAGE:\n", message) 
     
+# Example of a custom promise-handler - for tool execution
 def messageHandler_geoIp(result):
     print("\n\nGeo-IP Result: \n", result)
 
 api_key = "YOUR_API_KEY"
+
+# Create a ReconSession object - For development purposes, we are using wss://test.recon.us.com:2096, recon.us.com is not API-enabled yet. 
 rs = ReconSession(messageHandler_all, apiKey = api_key, onReady = onReady, api = "wss://test.recon.us.com:2096")
+
+# Test tool execution
 rs.tool_exec('geoip', 'google.com', handler = messageHandler_geoIp)
-          
