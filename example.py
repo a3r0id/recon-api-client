894e551b7ab3c2785955870cc73a3fd0e51e11da
A3R0 <50676042+hostinfodev@users.noreply.github.com>
2021-12-26 23:13:07 -0800

    Add files via upload

---
+++ example.py
@@ -0,0 +1,21 @@
+            
+import logging
+from datetime import datetime
+from recon_client import ReconSession
+
+# onReady handler         
+def onReady(tools):
+    logging.debug("Session established [%s]" % str(datetime.now()))
+    print("Session ready!")
+
+# main message handler
+def messageHandler_all(ws, message):
+    print("\n\nGOT MESSAGE:\n", message) 
+    
+def messageHandler_geoIp(result):
+    print("\n\nGeo-IP Result: \n", result)
+
+api_key = "YOUR_API_KEY"
+rs = ReconSession(messageHandler_all, apiKey = api_key, onReady = onReady, api = "wss://test.recon.us.com:2096")
+rs.tool_exec('geoip', 'google.com', handler = messageHandler_geoIp)
+          
